167153b4184945fb66995794bb96be8c688b29e9
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-26 14:05:03 +0800

    styling KeyFeatures component implement feature items from constant KEY_FEATURES_CONTENT, apply created framer motion variants

--- src/components/KeyFeatures.tsx
+++ src/components/KeyFeatures.tsx
@@ -1,4 +1,5 @@
 import { motion, type Variants } from "framer-motion";
+import { KEY_FEATURES_CONTENT } from "../constants";
 
 const containerVariants: Variants = {
   hidden: { opacity: 0 },
@@ -23,7 +24,37 @@ const featureVariants: Variants = {
 };
 
 const KeyFeatures = () => {
-  return <div></div>;
+  return (
+    <section className="max-w-7xl mx-auto px-4 mt-20">
+      <div className="text-center mb-12 border-t border-t-neutral-800">
+        <h2 className="text-3xl lg:text-5xl mt-20 tracking-tighter bg-gradient-to-t from-neutral-50 via-neutral-600 bg-clip-text text-transparent">
+          {KEY_FEATURES_CONTENT.sectionTitle}
+        </h2>
+        <p className="mt-4">{KEY_FEATURES_CONTENT.sectionDescription}</p>
+      </div>
+
+      <motion.div
+        variants={containerVariants}
+        initial="hidden"
+        whileInView="visible"
+        className="flex flex-wrap justify-between"
+      >
+        {KEY_FEATURES_CONTENT.features.map((f) => (
+          <motion.div
+            key={f.id}
+            variants={featureVariants}
+            className="flex flex-col items-center text-center w-full md:w-1/2 lg:w-1/3 p-6"
+          >
+            <div className="flex justify-center items-center mb-4">
+              {f.icon}
+            </div>
+            <h3 className="text-xl">{f.title}</h3>
+            <p className="mt-2 text-neutral-400">{f.description}</p>
+          </motion.div>
+        ))}
+      </motion.div>
+    </section>
+  );
 };
 
 export default KeyFeatures;
