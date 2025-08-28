2995449fbd8588c50dfc9fc82c38ae38216716d9
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-28 11:48:39 +0800

    styling Testimonial Header implement TESTIMONIALS_CONTENT constant title and description

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,4 +1,5 @@
 import { motion, type Variants } from "framer-motion";
+import { TESTIMONIALS_CONTENT } from "../constants";
 const Testimonials = () => {
   const testimonialVariants: Variants = {
     hidden: { opacity: 0, y: 50 },
@@ -14,8 +15,20 @@ const Testimonials = () => {
   };
 
   return (
-    <section id="testimonials">
-      <div className="bg-white max-w-7xl mx-auto">Testimonials</div>
+    <section id="testimonial">
+      <div className="max-w-7xl mx-auto px-4 mt-20">
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, ease: "easeOut" }}
+          className="text-center mb-12 border-t border-neutral-800"
+        >
+          <h2 className="text-3xl lg:text-5xl mt-20 tracking-tighter bg-gradient-to-t from-neutral-50 via-neutral-300 to-neutral-600 bg-clip-text text-transparent">
+            {TESTIMONIALS_CONTENT.sectionTitle}
+          </h2>
+          <p className="mt-4">{TESTIMONIALS_CONTENT.sectionDescription}</p>
+        </motion.div>
+      </div>
     </section>
   );
 };
