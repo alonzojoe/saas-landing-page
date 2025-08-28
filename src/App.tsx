3650a8ba87b125e7ee55593fe24f19afeebc4e8e
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-28 11:19:30 +0800

    create new Testimonials component implement i App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import HowItWorks from "./components/HowItWorks";
 import KeyFeatures from "./components/KeyFeatures";
 import PricingPlans from "./components/PricingPlans";
+import Testimonials from "./components/Testimonials";
 const App = () => {
   return (
     <main className="text-sm text-neutral-300 antialiased">
@@ -11,6 +12,7 @@ const App = () => {
       <HowItWorks />
       <KeyFeatures />
       <PricingPlans />
+      <Testimonials />
     </main>
   );
 };
---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,9 @@
+const Testimonials = () => {
+  return (
+    <section id="testimonials">
+      <div className="bg-white max-w-7xl mx-auto">Testimonials</div>
+    </section>
+  );
+};
+
+export default Testimonials;
