b84540ccfaa28383d567cfc3d65d1255db6869ed
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-28 11:26:12 +0800

    create testimonialVariants for animation  annotate type Variants from framer-motion

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,4 +1,18 @@
+import { motion, type Variants } from "framer-motion";
 const Testimonials = () => {
+  const testimonialVariants: Variants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: (i: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: i * 0.2,
+        duration: 0.6,
+        ease: "easeOut",
+      },
+    }),
+  };
+
   return (
     <section id="testimonials">
       <div className="bg-white max-w-7xl mx-auto">Testimonials</div>
